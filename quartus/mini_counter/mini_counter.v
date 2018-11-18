a45c60a7704561554b57dab055cdec92e63eb79f
Jose Luis Cueva <joseluis7012@gmail.com>
2018-11-18 21:01:19 -0200

    minor changes

--- quartus/mini_counter/mini_counter.v
+++ quartus/mini_counter/mini_counter.v
@@ -1,9 +1,16 @@
 module mini_counter(
 input clk,
-rst,
+input rst,
 output reg [31:0] cnt
 );
 
+always @(posedge clk or posedge rst)
+begin
+if (rst)
+  cnt = 0;
+else 
+  cnt = cnt + 1;
+end
 
 
 endmodule
